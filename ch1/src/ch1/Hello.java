254c2bf9771022f59caf19ef6a65194068136a62
유용균 <yuioo2000@naver.com>
2024-08-20 18:17:15 +0900

    test1

--- ch1/src/ch1/Hello.java
+++ ch1/src/ch1/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         // 메소드는 형태가 정해져있음 리턴 타입, 메소드명()
         System.out.println("Hello");
+        // wow jooseok
+        System.out.println("hi");
     }
 }
